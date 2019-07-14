f948ae8057962633015f136cb896786ae1970418
Max <ferromassimo1989@gmail.com>
2019-07-14 19:00:05 +0200

    update packages

---
+++ build/precache-manifest.f4bef4b70451ed43705525f75d25890a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ed6b282456b01f1d8455bdb4fb8100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04027c8c0921937c38cf",
+    "url": "/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "508c8c43bbde3e63c111",
+    "url": "/static/css/main.329c9cc3.chunk.css"
+  },
+  {
+    "revision": "04027c8c0921937c38cf",
+    "url": "/static/js/2.afcc91db.chunk.js"
+  },
+  {
+    "revision": "508c8c43bbde3e63c111",
+    "url": "/static/js/main.ca781815.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "858445aeac6e11c79002007550200053",
+    "url": "/static/media/halo_reach_wallpaper_1920x1080_by_hellhoundx666.858445ae.jpg"
+  },
+  {
+    "revision": "0d0e9d727922d36a2395bcce21be8991",
+    "url": "/static/media/love.0d0e9d72.jpg"
+  },
+  {
+    "revision": "4b7effa9d62bafac95bfd1c04c8f2463",
+    "url": "/static/media/wpp.4b7effa9.jpg"
+  }
+]);
